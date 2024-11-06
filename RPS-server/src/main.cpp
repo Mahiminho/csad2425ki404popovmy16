9ae0a782fa0288071568f69000f700ddf45b61f5
Maxim Popov <maximpopovdev@gmail.com>
2024-11-06 22:04:56 +0200

    Add trained neural network for server

--- RPS-server/src/main.cpp
+++ RPS-server/src/main.cpp
@@ -6,12 +6,33 @@
 
 using namespace tinyxml2;
 
+// addition parameters from my neural network
+std::vector<int> aiMemory = { 2, 1, 3 };
+float inputWeights[3][8] = {
+    { 0.237058, -0.290494, 0.274185, 0.002906, -0.130077, 0.604973, 0.687958, 0.113789 },
+    { 0.486323, -0.628314, 0.194522, 0.172590, 0.763265, -0.167007, 1.251667, 0.067055 },
+    { 0.120458, -0.396984, 1.712663, 1.075813, 0.168848, -0.612591, -1.263713, 1.474092 },
+};
+float hiddenWeights[8][3] = {
+    { 0.469825, -0.215859, -0.548794 },
+    { 0.285107, -0.448687, -0.552901 },
+    { 0.656990, -1.276879, 0.272193 },
+    { 1.293255, -1.585455, -1.156214 },
+    { -0.498069, 0.174363, 0.186442 },
+    { -0.631017, 1.110896, 0.074588 },
+    { -0.578895, 0.978867, 0.364652 },
+    { 1.034574, -1.109686, 0.851510 },
+};
+float hiddenBias[8] = {-0.376692, 0.000000, -0.370774, -0.863258, 0.907450, 0.770704, 1.312167, -0.110566};
+float outputBias[3] = {-0.725252, 0.632030, 0.405089};
+
+// addition functions for working
+// with data from my neural network
+float Relu(float x);
+void SoftMax(float x[], int size);
+String SmartChoiceAI();
 // get result of game
 String chooseWinner(String p1, String p2);
-// array with most popular opponent choices (last 6 games)
-std::vector<String> aiMemory = { "1", "2", "3", "1", "2", "3" }; //, "1", "2", "3" };
-// function for AI choice based on its memory
-String smartChoiceAI(const std::vector<String>& vec);
 
 void setup() {
   Serial.begin(9600);
@@ -52,13 +73,13 @@ void loop() {
     // process
     if (modeVal == "1") {
       // smart choice by AI
-      String ai1Choice = smartChoiceAI(aiMemory);
+      String ai1Choice = SmartChoiceAI();
 
       String winner = chooseWinner(p1Val, ai1Choice);
 
       // update AI memory after game
       aiMemory.pop_back();
-      aiMemory.insert(aiMemory.begin(), p1Val);
+      aiMemory.insert(aiMemory.begin(), p1Val.toInt());
 
       ai1->SetText(ai1Choice.c_str());
       win->SetText(winner.c_str());
@@ -81,13 +102,13 @@ void loop() {
       // random choice by AI1
       String ai1Choice = String(random(1, 4));
       // smart choice by AI2
-      String ai2Choice = smartChoiceAI(aiMemory);
+      String ai2Choice = SmartChoiceAI();
 
       String winner = chooseWinner(ai1Choice, ai2Choice);
 
       // update AI memory after game
       aiMemory.pop_back();
-      aiMemory.insert(aiMemory.begin(), p1Val);
+      aiMemory.insert(aiMemory.begin(), ai1Choice.toInt());
 
       ai1->SetText(ai1Choice.c_str());
       ai2->SetText(ai2Choice.c_str());
@@ -119,18 +140,55 @@ String chooseWinner(String p1, String p2) {
   return "5";
 }
 
-String smartChoiceAI(const std::vector<String>& vec) {
-  std::map<String, int> frequency;
-  for (String val : vec)
-    frequency[val]++;
-  
-  String mostPopularChoice = std::max_element(frequency.begin(), frequency.end(),
-    [](const auto& a, const auto& b) { return a.second < b.second; })->first;
+float Relu(float x)
+{
+    return x > 0 ? x : 0;
+}
 
-  if (mostPopularChoice == "1")
-    return "2";
-  else if (mostPopularChoice == "2")
-    return "3";
-  else
-    return "1";
+void SoftMax(float x[], int size)
+{
+    float max_val = x[0];
+    for (int i = 1; i < size; i++)
+        if (x[i] > max_val)
+            max_val = x[i];
+    float sum = 0;
+    for (int i = 0; i < size; i++)
+        sum += exp(x[i] - max_val);
+    for (int i = 0; i < size; i++)
+        x[i] = exp(x[i] - max_val) / sum;
+}
+
+String SmartChoiceAI()
+{
+    float output[3];
+
+    float hiddenLayer[8];
+    for (int i = 0; i < 8; i++)
+    {
+        hiddenLayer[i] = hiddenBias[i];
+        for (int j = 0; j < 3; j++)
+        {
+            hiddenLayer[i] += aiMemory[j] * inputWeights[j][i];
+        }
+        hiddenLayer[i] = Relu(hiddenLayer[i]);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        output[i] = outputBias[i];
+        for (int j = 0; j < 8; j++)
+        {
+            output[i] += hiddenLayer[j] * hiddenWeights[j][i];
+        }
+    }
+
+    SoftMax(output, 3);
+
+    int best_move = 0;
+    if (output[1] > output[best_move])
+        best_move = 1;
+    if (output[2] > output[best_move])
+        best_move = 2;
+
+    return String(best_move + 1);
 }
